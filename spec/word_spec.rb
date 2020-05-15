0aa595835794da97b7ff26f1271b9d4e683992eb
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-15 11:56:08 -0700

    fail sixth spec

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -46,4 +46,13 @@ describe('#Word.all') do
       expect(word1).to(eq(word1))
     end
   end
+  describe('.find') do
+    it("finds a word by id") do
+      word1 = Word.new({:term => "Dog", :id => nil})
+      word1.save()
+      word2 = Word.new({:term => "Cat", :id => nil})
+      word2.save()
+      expect(Word.find(word1.id)).to(eq(word1))
+    end
+  end
 end 
