097a15904f2cdad6c3d1f232af9d774fa7018d11
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-15 11:22:46 -0700

    fail second spec

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,10 +9,16 @@ before(:each) do
   Word.clear()
 end
 
-describe('.all') do 
+describe('#Word.all') do 
   it("returns an empty array when there are no words entered") do 
     expect(Word.all).to(eq([]))
-    
+
    end
   end 
+  describe('#term') do 
+    it("returns the term of a given word") do 
+      word = Word.new({:term =>"Dog"})
+      expect(word.term()).to(eq("Dog"))
+    end
+  end
 end 
