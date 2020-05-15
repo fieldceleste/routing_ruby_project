faa5fa877642136d6371a2f9ea41f080c049c59b
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-15 11:47:17 -0700

    pass fourth spec

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -29,14 +29,14 @@ describe('#Word.all') do
       expect(Word.all).to(eq([word1, word2]))
     end
   end
-  # describe('.clear') do
-  #   it("clears all entered words") do
-  #     word1 = Word.new({:term =>"Cat", :id => nil})
-  #     word1.save()
-  #     word2 = Word.new({:term =>"Hamster", :id => nil})
-  #     word2.save()
-  #     Word.clear()
-  #     expect(Word.all).to(eq([]))
-  #   end
-  # end
+  describe('.clear') do
+    it("clears all entered words") do
+      word1 = Word.new({:term =>"Cat", :id => nil})
+      word1.save()
+      word2 = Word.new({:term =>"Hamster", :id => nil})
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
 end 
