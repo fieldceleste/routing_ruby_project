ff626f149361f4f89dd8002c76fd916498b73fcf
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-25 15:47:14 -0700

    pass 1 integration spec

--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -5,13 +5,36 @@ require 'definition.rb'
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('create a word path', {:type => :feature}) do
-#   it('creates a word then goes to the word page') do
-#     visit('/words_homepage')
-#     save_and_open_page
-#     click_on('Add a new word')
-#     fill_in('word_name', :with => 'Dog')
-#     click_on('Go!')
-#     expect(page).to have_content('Dog')
+describe('create a word path', {:type => :feature}) do
+  it('creates a word then goes to the word page') do
+    visit('/words')
+    click_on('Add a new word!')
+    fill_in('word_term', :with => 'Dog')
+    click_on('Go!')
+    expect(page).to have_content('Dog')
+  end
+end
+
+# describe('create a definiton path', {:type => :feature}) do
+#   it('creates an word and then goes to the word page') do
+#     word = Word.new("Apple", nil)
+#     word.save
+#     visit("/words/#{word.id}")
+#     fill_in('definition_name', :with => 'A fruit')
+#     click_on('Add')
+#     expect(page).to have_content('A fruit')
+#   end
+# end
+
+# describe('Renames a definiton path', {:type => :feature}) do
+#   it('Renames an word and then goes to the word page') do
+#     word = Word.new("Apple", nil)
+#     word.save
+#     visit("/words/#{word.id}")
+#     click_on('Edit word')
+#     visit("/words/#{word.id}/edit")
+#     fill_in('name', :with => 'Orange')
+#     click_on('Update')
+#     expect(page).to have_content('Orange')
 #   end
 # end
