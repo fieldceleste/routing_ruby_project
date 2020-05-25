ecb7dbb51ca4a690dd07b008672237118bd67014
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-05-25 15:59:56 -0700

    pass 3 integration spec

--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -27,15 +27,15 @@ describe('create a definiton path', {:type => :feature}) do
   end
 end
 
-# describe('Renames a definiton path', {:type => :feature}) do
-#   it('Renames an word and then goes to the word page') do
-#     word = Word.new("Apple", nil)
-#     word.save
-#     visit("/words/#{word.id}")
-#     click_on('Edit word')
-#     visit("/words/#{word.id}/edit")
-#     fill_in('name', :with => 'Orange')
-#     click_on('Update')
-#     expect(page).to have_content('Orange')
-#   end
-# end
+describe('Renames a definiton path', {:type => :feature}) do
+  it('Renames an word and then goes to the word page') do
+    word = Word.new({:term => "Cat", :id => nil})
+    word.save()
+    visit("/words/#{word.id}")
+    click_on('Edit Word')
+    visit("/words/#{word.id}/edit")
+    fill_in('term', :with => 'Orange')
+    click_on('Update word-(Definition will stay the same')
+    expect(page).to have_content('Orange')
+  end
+end
